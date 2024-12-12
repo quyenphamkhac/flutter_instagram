e4e405d87e0fc080283eeac04e4c68f617d2ddf6
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2024-12-12 14:54:08 +0700

    feat: login page

--- lib/src/features/auth/presentation/pages/login_page.dart
+++ lib/src/features/auth/presentation/pages/login_page.dart
@@ -1,33 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram/src/core/router/router.dart';
+import 'package:flutter_svg/svg.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    final OutlineInputBorder inputBorder = OutlineInputBorder(
+      borderSide: Divider.createBorderSide(context),
+    );
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Login form fields would go here
-            ElevatedButton(
-              onPressed: () {
-                // Example navigation to feeds page after login
-                AppRouter.replaceWith(context, RouteNames.feeds);
-              },
-              child: Text('Login'),
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SvgPicture.asset('assets/icons/ic_instagram.svg'),
+                SizedBox(height: 8),
+                TextField(
+                  controller: _emailController,
+                  decoration: InputDecoration(
+                    hintText: 'Email',
+                    border: inputBorder,
+                    contentPadding: EdgeInsets.all(8),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    border: inputBorder,
+                    contentPadding: EdgeInsets.all(8),
+                  ),
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                ),
+                SizedBox(height: 8),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue.shade500,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    minimumSize: Size(double.infinity, 35.0),
+                  ),
+                  onPressed: () {
+                    AppRouter.replaceWith(context, RouteNames.feeds);
+                  },
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            TextButton(
-              onPressed: () {
-                // Navigate to signup page
-                AppRouter.navigateTo(context, RouteNames.signup);
-              },
-              child: Text('Sign Up'),
-            ),
-          ],
+          ),
         ),
       ),
     );
